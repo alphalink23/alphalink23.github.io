29b9b483b7e8b77535bf7d7add7cde03e1c9c659
mozaldy <moxaldy@gmail.com>
2023-11-14 16:54:47 +0700

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { resolve } from 'path'
-
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about/index.html'),
-        gallery: resolve(__dirname, 'src/gallery/index.html'),
-        members: resolve(__dirname, 'src/members/index.html')
-      },
-    },
-  },
 })
