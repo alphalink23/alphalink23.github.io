9b498be7ca7b20b7f31ae6f124cb05e3e85a05a1
mozaldy <moxaldy@gmail.com>
2023-11-20 12:12:52 +0700

    deploy

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: '/alphalink/',
-  plugins: [vue({
-    isProduction: true
-  })],
-})
